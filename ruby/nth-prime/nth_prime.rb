ee19884595733a5e919c4344d50a1f0bbd0489f0
Nick DelRossi <nickwtf@gmail.com>
2015-04-18 09:26:06 -0400

    Submit ruby nth-prime

---
+++ ruby/nth-prime/nth_prime.rb
@@ -0,0 +1,21 @@
+class Prime
+  def nth(num, index = 2)
+    raise ArgumentError if num < 1
+    while num > 0 do
+      if isPrime?(index)
+        return index if num == 1
+        num -= 1
+      end
+      index += 1
+    end
+  end
+
+  private
+
+  def isPrime?(num)
+    (2...num).each do |i|
+      return false if num % i == 0
+    end
+    true
+  end
+end
